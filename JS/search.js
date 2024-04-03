5d4c8e416c74b1a9d45fb7b4193c7a2cca3e63ef
Diegody <diegogody12@gmail.com>
2024-04-03 18:48:33 -0500

    Excepciones

--- JS/search.js
+++ JS/search.js
@@ -18,12 +18,16 @@ document.addEventListener('DOMContentLoaded', function() {
                         // Mostrar los datos en la interfaz
                         renderData(data, token); // Pasar el token como argumento
                     } else {
-                        searchResults.innerHTML = `<p>No se encontraron datos para el token <strong>${token}</strong>.</p>`;
+                        searchResults.innerHTML = `
+                        <p style='color: red;'><b>DATOS ASOCIADOS AL TOKEN NO ENCONTRADOS</b></p>
+                        <p>No se encontraron datos para el token <strong>${token}</strong>.</p>`;
                     }
                 })
                 .catch(error => {
                     console.error('Error al buscar datos:', error);
-                    searchResults.innerHTML = `<p>Ocurrió un error al buscar datos para el token <strong>${token}</strong>.</p>`;
+                    searchResults.innerHTML = `
+                    <p style='color: red;'><b>ERROR AL REALIZAR LA BUSQUEDA</b></p>
+                    <p>Ocurrió un error al buscar datos para el token <strong>${token}</strong>.</p>`;
                 });
         } else {
             alert('Por favor ingrese un token válido.');
@@ -33,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function() {
     function renderData(data, token) { // Agregar token como argumento
         // Mostrar los datos en la interfaz
         searchResults.innerHTML = `
-        <p><b>ESQUEMA GENERADO EXITOSAMENTE</b></p>
+        <p style='color: green;'><b>ESQUEMA GENERADO EXITOSAMENTE</b></p>
         <p>Resultados para el token <strong>${token}</strong>.</p>
         `;
         for (const key in data) {
